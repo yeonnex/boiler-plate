176e97c5492ce0626f82eb3937b97ce426b210e5
yeonnex <yeonnex@gmail.com>
2021-07-13 06:45:11 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seoyeon:abcd1234@boilerplate.zk8a9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.set('useCreateIndex', true);
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true,useUnifiedTopology: true 
 }).then(()=>console.log("MongoDB connected..."))
     .catch(err => console.log(err))
+
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json
+app.use(bodyParser.json());
+
+
+
 app.get('/', (req, res) => {
-  res.send('엔젤리너스 반미불고기 바게트 개꿀맛!')
+  res.send('엔젤리너스 반미불고기 바게트 JMT!')
 })
 
+
+
+app.post('/register', (req,res)=>{
+    // 클라이언트에서 보내온 회원가입시의 정보들을
+    // 데이터베이스에 넣어준다
+
+    const user = new User(req.body) // 바디파서때문에 가능!
+    
+    user.save((err,userInfo)=>{// save()메서드는 몽고디비에서 오는 메서드임!
+        if(err) return res.json({success:false,err })
+        return res.status(200).json({
+            success:true
+        })    
+    })
+}) 
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`My app listening at http://localhost:${port}`)
 })
